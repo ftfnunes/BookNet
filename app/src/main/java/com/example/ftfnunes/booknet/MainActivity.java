7faa987197595ec2394fc158347bfc24ba8126e1
Fabio Marques <140039082@aluno.unb.br>
2016-10-29 21:00:37 -0200

    Menu lateral funcional nas telas iniciais e de anuncio

--- app/src/main/java/com/example/ftfnunes/booknet/MainActivity.java
+++ app/src/main/java/com/example/ftfnunes/booknet/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.ftfnunes.booknet;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
 
     @Override
