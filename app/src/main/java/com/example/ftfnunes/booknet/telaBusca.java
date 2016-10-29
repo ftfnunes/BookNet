cf440969e0309386ede78b33e59e9905fb81c789
Fabio Marques <140039082@aluno.unb.br>
2016-10-29 17:52:53 -0200

    Adicionada a tela de busca

---
+++ app/src/main/java/com/example/ftfnunes/booknet/telaBusca.java
@@ -0,0 +1,13 @@
+package com.example.ftfnunes.booknet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class telaBusca extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_busca);
+    }
+}
