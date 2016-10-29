82cf722cf37935b6c386b777b43b7dee93e86519
Fabio Marques <140039082@aluno.unb.br>
2016-10-29 02:51:55 -0200

    Adição da tela de login, ainda inacabada e testes na MainActivity

--- app/src/main/java/com/example/ftfnunes/booknet/MainActivity.java
+++ app/src/main/java/com/example/ftfnunes/booknet/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.ftfnunes.booknet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button botaoTeste = (Button) findViewById(R.id.botaoTeste);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void noClick(View v) {
+        Intent it = new Intent(MainActivity.this, telaLogin.class);
+        startActivity(it);
     }
 }
