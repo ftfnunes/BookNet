ab880bbfa7115a64e3c3b629dfc88513a7e3985a
Bruno Rodrigues <brunin2509@gmail.com>
2016-10-30 03:18:08 -0200

    Mais duas telas

---
+++ app/src/main/java/com/example/ftfnunes/booknet/AprovacaoDeSolicitacao.java
@@ -0,0 +1,13 @@
+package com.example.ftfnunes.booknet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AprovacaoDeSolicitacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprovacao_de_solicitacao);
+    }
+}
---
+++ app/src/main/java/com/example/ftfnunes/booknet/LivroEmprestado.java
@@ -0,0 +1,13 @@
+package com.example.ftfnunes.booknet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LivroEmprestado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livro_emprestado);
+    }
+}
